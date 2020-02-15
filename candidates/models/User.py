2c3242486d61196e26e30563f3320b68f6e07777
Mustaghees Butt <renaldpirate@gmail.com>
2020-02-15 18:18:19 +0500

    worked on dashboard

--- candidates/models/User.py
+++ candidates/models/User.py
@@ -3,11 +3,12 @@ from django.utils import timezone as tz
 
 
 class User(models.Model):
-    email      =  models.EmailField('email',max_length=40,unique=True)
-    password   =  models.CharField('password',null=False,max_length=255)
-    role       =  models.CharField('role',max_length=20)
+    email      = models.EmailField('email',max_length=40,unique=True)
+    password   = models.CharField('password',null=False,max_length=255)
+    role       = models.CharField('role',max_length=20)
     created_at = models.DateField(default=tz.now)
     updated_at = models.DateField(default=tz.now)
+    
     class Meta:
         app_label = 'candidates'
 
--- candidates/views.py
+++ candidates/views.py
@@ -15,3 +15,15 @@ def testing(request):
         "status": status
     }
     return HttpResponse(json.dumps(obj), content_type="application/json", status=200)
+
+def entry_test_application(request):
+    return render(request, "pages/candidate/entry_test_application.html")
+
+def registeration_slip(request):
+    return render(request, "pages/candidate/registeration_slip.html")
+
+def adjust_test_schedule(request):
+    return render(request, "pages/candidate/adjust_test_schedule.html")
+
+def entry_test_result(request):
+    return render(request, "pages/candidate/entry_test_result.html")
