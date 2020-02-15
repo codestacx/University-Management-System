a70f7360d704889ec3a2322b6f1f0fec1638c9f5
Mustaghees Butt <renaldpirate@gmail.com>
2020-02-15 14:06:06 +0500

    error pages added

--- candidates/views.py
+++ candidates/views.py
@@ -1,14 +1,17 @@
 from django.http import HttpResponse,JsonResponse
 from django.shortcuts import render
+from django.template import RequestContext
 from candidates.Controllers.Email import sendMail
 from candidates.models.VerificationCode import VerificationCode
 import json
+
+
 def testing(request):
-    email=request.POST['email']
-    code = request.POST['code']
-    count = VerificationCode.objects.filter(email=email,code=code).count()
-    status = True if count>0 else False
-    obj={
-        "status":status
+    email  = request.POST['email']
+    code   = request.POST['code']
+    count  = VerificationCode.objects.filter(email=email, code=code).count()
+    status = True if count > 0 else False
+    obj = {
+        "status": status
     }
     return HttpResponse(json.dumps(obj), content_type="application/json", status=200)
