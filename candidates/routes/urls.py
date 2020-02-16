46d6e19748fce95e7da63804f9ebde5067465206
muhammad atif <muhammadatif.pucit@gmail.com>
2020-02-16 23:38:31 +0500

    fucking work done by teamates

--- candidates/routes/urls.py
+++ candidates/routes/urls.py
@@ -21,9 +21,13 @@ urlpatterns=[
     path('verifymail',sendMail.verifyEmail,name='verifymail'),
     path('resetpasswords/',sendMail.resetPassword,name='resetpasswords'),
     path('confirmresetpassword/<int:user_id>',sendMail.resetPassword,name='confirmresetpassword' ),
-    path('submitresetpassword',sendMail.resetPassword,name='submitresetpassword'),
+    path('submitresetpassword',sendMail.submitResetPassword,name='submitresetpassword'),
 
     path('testing/',views.testing,name='testing'),
+    path("entry-test-application", views.entry_test_application, name='entry_test_application'),
+    path("registeration-slip", views.registeration_slip, name='registeration_slip'),
+    path("adjust-test-schedule", views.adjust_test_schedule, name='adjust_test_schedule'),
+    path("entry-test-result", views.entry_test_result, name='entry_test_result')
 
 
 ]
