164c1f108aa2238e8cf69ced901b68c33f52873b
JackSparroow <muhammadatif.pucit@gmail.com>
2020-06-11 12:56:22 +0500

    done

--- admin/routes/urls.py
+++ admin/routes/urls.py
@@ -6,6 +6,8 @@ from admin.Controllers import ChallanController
 from admin.Controllers import EntryTestController
 from admin.Controllers import QualificationController
 urlpatterns = [
+
+
     path('login/', AuthController.index, name='admin_login'),
     path('signout', AuthController.logout, name='admin_logout'),
 
